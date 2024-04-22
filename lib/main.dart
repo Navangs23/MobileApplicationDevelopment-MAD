17233504682b8990325d0b5d911ca3bdb76cbfe5
Navangs23 <navanshah2303#gmail.com>
2024-04-22 19:43:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'dart:ui' show PointerDeviceKind;
+import 'package:e_commerce_flutter/core/app_theme.dart';
+import 'package:e_commerce_flutter/src/view/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+      theme: AppTheme.lightAppTheme,
+    );
+  }
+}
